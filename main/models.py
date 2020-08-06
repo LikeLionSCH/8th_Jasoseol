48663ca273caa5636a67d32879213bd13f1f79f4
ningpop <craft1933@naver.com>
2020-08-06 23:22:17 +0900

    Add Comment

--- main/models.py
+++ main/models.py
@@ -17,3 +17,12 @@ class Jasoseol(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    jasoseol = models.ForeignKey(Jasoseol, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+    
