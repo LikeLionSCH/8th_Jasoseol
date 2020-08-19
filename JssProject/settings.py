4fdfb7ebf52c8c25ce6c252ed266dff4af18d765
ningpop <craft1933@naver.com>
2020-08-19 18:16:01 +0900

    ready for heroku

--- JssProject/settings.py
+++ JssProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,5 +151,6 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
